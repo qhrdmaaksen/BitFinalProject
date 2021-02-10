792a9751f147c214a3408f9c3bd4e530526ab2c1
홍경희 <ghdrudgml90@naver.com>
2021-02-10 11:50:41 +0900

    productreviews 빈

---
+++ bitShopping/src/main/java/bean/Productreviews.java
@@ -0,0 +1,17 @@
+package bean;
+
+public class Productreviews {
+	
+	private int bno; // 상품 후기 글 번호 
+	private String btitle; // 상품 후기 글 제목 
+	private String bwriter; // 상품 후기 글 작성자 
+	private String bcontent; // 상품 후기 글 내용 
+	private String image1; // 상품 후기 이미지 1
+	private String image2; // 상품 후기 이미지 2
+	private String image3; // 상품 후기 이미지 3
+	private int breview; // 상품 후기 조회수 
+	private String bregdate; // 상품 후기 작성 일자 
+	
+	
+
+}
